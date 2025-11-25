ef8799e631ae01901f42aad5380375ff01bdeca9
Rachana R Tunga <147418029+Racha2005@users.noreply.github.com>
2025-11-25 12:16:26 +0530

    Update config.py

--- music_prediction_recommendation_analysis_project/music_app/config.py
+++ music_prediction_recommendation_analysis_project/music_app/config.py
@@ -1,1 +1,12 @@
-DATA_PATH = 'data/music_dataset_500.csv'
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Dataset
+DATA_LOCAL = os.path.abspath(os.path.join(BASE_DIR, "..", "data", "music_dataset_500.csv"))
+DATA_FALLBACK = DATA_LOCAL  # fallback same file
+
+# Model paths
+MODEL_DIR = os.path.join(BASE_DIR, "models")
+MODEL_POP = os.path.join(MODEL_DIR, "popularity_rf.pkl")
+MODEL_MOOD = os.path.join(MODEL_DIR, "mood_clf.pkl")
